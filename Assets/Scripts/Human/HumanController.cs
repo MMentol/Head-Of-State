bbf2aeb5786fbd69a66057ccfa08e3150c282e79
MMentol <112599969+MMentol@users.noreply.github.com>
2024-07-29 22:44:55 +0800

    Added Movement to Human

--- Assets/Scripts/Human/HumanController.cs
+++ Assets/Scripts/Human/HumanController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class HumanController : MonoBehaviour
 {
+    private Rigidbody body;
+    private Animator anim;
+    private BoxCollider boxCollider;
+    private float horizontalInput;
+    private float verticalInput;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Grab references for rigidbody and animator from object
+        body = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
     }
 }

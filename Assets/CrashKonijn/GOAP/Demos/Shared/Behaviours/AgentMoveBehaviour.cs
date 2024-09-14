610174cd5c3d698d93dcc9bc1537ca2b68c242b2
MMentol <112599969+MMentol@users.noreply.github.com>
2024-09-14 15:06:36 +0800

    AgentMove

--- Assets/CrashKonijn/GOAP/Demos/Shared/Behaviours/AgentMoveBehaviour.cs
+++ Assets/CrashKonijn/GOAP/Demos/Shared/Behaviours/AgentMoveBehaviour.cs
@@ -56,8 +56,8 @@ namespace Demos.Shared.Behaviours
             
             if (this.currentTarget == null)
                 return;
-            humanController.SetTargetPosition(new Vector3(this.currentTarget.Position.x, this.transform.position.y));
-            //this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(this.currentTarget.Position.x, this.transform.position.y, this.currentTarget.Position.z), Time.deltaTime);
+            //humanController.SetTargetPosition(new Vector3(this.currentTarget.Position.x, this.transform.position.y));
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(this.currentTarget.Position.x, this.transform.position.y, this.currentTarget.Position.z), Time.deltaTime);
         }
 
         private void OnDrawGizmos()

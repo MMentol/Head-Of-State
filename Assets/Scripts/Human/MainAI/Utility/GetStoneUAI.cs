16ddcbcf9a2cd648d582e4d449461e3345c480af
MMentol <112599969+MMentol@users.noreply.github.com>
2024-09-22 14:39:29 +0800

    UAIFixing

---
+++ Assets/Scripts/Human/MainAI/Utility/GetStoneUAI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetStoneUAI : AgentAction
+{
+    public GetStoneUAI()
+    { 
+    }
+
+    public override void OnStart(IAiContext context)
+    {
+        var agent = context.Agent as AgentMono;
+    }
+    // The OnGoing method is called on every consecutive selection
+    public override void OnGoing(IAiContext context)
+    {
+        var agent = context.Agent as AgentMono;
+
+    }
+
+    // The OnEnd method is called, when the decision was selected last evaluation, but not this evaluation
+    public override void OnEnd(IAiContext context)
+    {
+        var agent = context.Agent as AgentMono;
+    }
+}

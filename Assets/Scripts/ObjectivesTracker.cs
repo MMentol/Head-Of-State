b1c42c6a0d3aac75e80e14efab17ee34d9d6908e
Ethan-CICS <ejlu10@gmail.com>
2024-09-29 01:24:55 +0800

    Fixed Objective Tracker
    It only prints once now :de:

--- Assets/Scripts/ObjectivesTracker.cs
+++ Assets/Scripts/ObjectivesTracker.cs
@@ -15,6 +15,7 @@ public class ObjectivesTracker : MonoBehaviour
     [SerializeField] UIScript uis = null;
 
     float Pop, Wood, Stone, Metal;
+    int objCount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,54 +41,54 @@ public class ObjectivesTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int objCount = 0;
+        bool printed = false;
         // Objective 1
-        if(Wood == 10) {
+        if(Wood == 10 && objCount == 0) {
             WriteCSV(1, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 2
-        if(Pop >= 5 && Wood >= 30 && Stone >= 10) { 
+        if(Pop >= 5 && Wood >= 30 && Stone >= 10 && objCount == 1) { 
             WriteCSV(2, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 3
-        if(Pop >= 10 && mds.Metal >= 5) {
+        if(Pop >= 10 && mds.Metal >= 5 && objCount == 2) {
             WriteCSV(3, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 4
-        if(Pop >= 14 && Wood >= 60 && Stone >= 20) {
+        if(Pop >= 14 && Wood >= 60 && Stone >= 20 && objCount == 3) {
             WriteCSV(4, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 5
-        if(Stone >= 40 && Metal >= 30){
+        if(Stone >= 40 && Metal >= 30 && objCount == 4){
             WriteCSV(5, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 6
-        if(Wood >= 100 && Metal >= 50){
+        if(Wood >= 100 && Metal >= 50 && objCount == 5){
             WriteCSV(6, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 7
-        if(Stone >= 100 && Metal >= 70){
+        if(Stone >= 100 && Metal >= 70 && objCount == 6){
             WriteCSV(7, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 8
-        if(Wood >= 150 && Stone >= 120) {
+        if(Wood >= 150 && Stone >= 120 && objCount == 7) {
             WriteCSV(8, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 9
-        if(Wood >= 150 && Stone >= 120 && Metal >= 100) {
+        if(Wood >= 150 && Stone >= 120 && Metal >= 100 && objCount == 8) {
             WriteCSV(9, uis.getTime(uis.time));
             objCount++;
         }
         // Objective 10
-        if(Pop >= 60) {
+        if(Pop >= 60 && objCount == 9) {
             WriteCSV(10, uis.getTime(uis.time));
             objCount++;
         }
@@ -101,8 +102,10 @@ public class ObjectivesTracker : MonoBehaviour
 
     public void WriteCSV(int objNo, string time) {
         TextWriter tw = new StreamWriter(filename, true);
-            tw.WriteLine(objNo + " ," + time);
-            tw.Close();
+        tw.WriteLine(objNo + " ," + time);
+        tw.Close();
+        Debug.Log(objNo + " ," + time);
+        return;
     }
 
 }

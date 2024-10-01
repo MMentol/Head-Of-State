706c1782c8c53030bfc447fa7a387cfcf620d4b0
Ethan-CICS <ejlu10@gmail.com>
2024-10-01 21:58:11 +0800

    CameraUpdate

--- Assets/Scripts/UIScripts/CameraMovement.cs
+++ Assets/Scripts/UIScripts/CameraMovement.cs
@@ -6,32 +6,46 @@ public class CameraMovement : MonoBehaviour
 {
     Camera currCam;
     float ScrollSpeed = 10f;
-    float moveSpeed = 20f;
-    // Start is called before the first frame update
-    void Start()
+    float RectangleScrollSpeed = 2f;
+    float moveSpeed = 2.4f;
+    float maxRect = 40f;
+    Transform spriteTransform = null;
+
+    void Awake()
     {
         currCam = Camera.main;
+        spriteTransform = this.gameObject.transform.GetChild(0).gameObject.GetComponentInChildren<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(currCam.orthographic) {
-            float maxOrtho = currCam.orthographicSize;
-            maxOrtho -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
-            currCam.orthographicSize = Mathf.Clamp(maxOrtho, 5, 40);
-            // Debug.Log(currCam.orthographicSize);
+            zoomRect();
+            zoomCam();
         }
         else {
             currCam.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
             Debug.Log(currCam.fieldOfView);
         }
-        float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float y = Input.GetAxis("Vertical") * moveSpeed;
         float xPos = transform.localPosition.x + x;
         float yPos = transform.localPosition.y + y;
         float xBound = Mathf.Clamp(xPos, -140, 50);
         float yBound = Mathf.Clamp(yPos, 35, 115);
         gameObject.transform.position = new Vector3(xBound, yBound, transform.position.z);
+    }
+
+    void zoomCam(){
+        float maxOrtho = currCam.orthographicSize;
+        maxOrtho -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
+        currCam.orthographicSize = Mathf.Clamp(maxOrtho, 5, 40);
+    }
+    void zoomRect(){
+        float maxRect = currCam.orthographicSize;
+        maxRect -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
+        float clampedRect = Mathf.Clamp(maxRect, 5, 40);
+        spriteTransform.localScale = new Vector3(clampedRect, clampedRect, 1);
     }
 }

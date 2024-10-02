273e9141275e354c6553847438770072aa6bd078
MMentol <112599969+MMentol@users.noreply.github.com>
2024-10-03 02:05:47 +0800

    Does not vibe into water during Idle

--- Assets/Scripts/Human/MainAI/GOAP/Sensors/WanderTargetSensor.cs
+++ Assets/Scripts/Human/MainAI/GOAP/Sensors/WanderTargetSensor.cs
@@ -19,8 +19,13 @@ public class WanderTargetSensor : LocalTargetSensorBase
     // Called when the sensor needs to sense a target for a specific agent.
     public override ITarget Sense(IMonoAgent agent, IComponentReference references)
     {
+        var tilemap = GameObject.FindFirstObjectByType<GridManager>();
         var random = this.GetRandomPosition(agent);
-
+        while (tilemap.GetTileAtPos(new Vector2(random.x, random.z), false) == null || !(tilemap.GetTileAtPos(new Vector2(random.x, random.z), false).isWalkable))
+        {
+            random = this.GetRandomPosition(agent);
+        }
+        Debug.Log("randPos: " + tilemap.GetTileAtPos(new Vector2(random.x, random.z), false));
         return new PositionTarget(random);
     }
 
@@ -28,7 +33,7 @@ public class WanderTargetSensor : LocalTargetSensorBase
     {
         var random = Random.insideUnitCircle * 5f;
         var position = agent.transform.position + new Vector3(Mathf.Clamp(Mathf.RoundToInt(random.x), 0, 99), 0f, Mathf.Clamp(Mathf.RoundToInt(random.y), 0, 99));
-
-        return position;
+        
+        return new Vector3(Mathf.Clamp(Mathf.RoundToInt(position.x), 0, 99), 0f, Mathf.Clamp(Mathf.RoundToInt(position.z), 0, 99));
     }
 }

5072cd2ae8f2f1627012992070c552dfd5f6812c
MMentol <112599969+MMentol@users.noreply.github.com>
2024-10-02 13:29:47 +0800

    Made happiness to fuck threshold lower

--- Assets/Scripts/Cinaed/GOAP Complex/Factories/HouseActivitySettings.cs
+++ Assets/Scripts/Cinaed/GOAP Complex/Factories/HouseActivitySettings.cs
@@ -4,7 +4,7 @@ namespace Cinaed.GOAP.Complex.Factories
 {
     public class HouseActivitySettings : MonoBehaviour
     {
-        public float RequiredHappiness = 100f;
+        public float RequiredHappiness = 80f;
         public float RestingEnergyBenefit = 100f;
     }
 }

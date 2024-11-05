3eb7d9a0cae0f4c35478c4817d887564116539a5
MMentol <112599969+MMentol@users.noreply.github.com>
2024-11-05 23:48:20 +0800

    Should not spawn on water

--- Assets/Scripts/Cinaed/GOAP Complex/AgentSpawner.cs
+++ Assets/Scripts/Cinaed/GOAP Complex/AgentSpawner.cs
@@ -65,7 +65,7 @@ namespace Cinaed.GOAP.Complex
         private void SpawnAgent(string setId, Color color)
         {
             var agent = Instantiate(this.agentPrefab, this.GetRandomPosition(), Quaternion.identity, gameObject.transform);
-
+            
             if (!useBT){ 
                 agent.GetComponent<AgentBehaviour>().GoapSet = this.goapRunner.GetGoapSet(setId);
                 agent.GetComponent<TextUpdate>().goap = true;
@@ -90,15 +90,19 @@ namespace Cinaed.GOAP.Complex
 
             if (useCustomBounds)
             {
-                if (customBounds.x > grid.GetWidth() - 1)
-                    customBounds.x = grid.GetWidth() - 1;
-                if (customBounds.y > grid.GetHeight() - 1)
-                    customBounds.y = grid.GetHeight() - 1;
+                Tile tile;
+                do
+                {
+                    if (customBounds.x > grid.GetWidth() - 1)
+                        customBounds.x = grid.GetWidth() - 1;
+                    if (customBounds.y > grid.GetHeight() - 1)
+                        customBounds.y = grid.GetHeight() - 1;
 
-                randomX = Random.Range(40, customBounds.x + 40);
-                randomY = Random.Range(40, customBounds.y + 40);
-                //Debug.Log($"CUSTOM X: {randomX}, Y: {randomY}");
-                Tile tile = grid.GetTileAtPos(new Vector2(Mathf.Round(randomX), Mathf.Round(randomY)));
+                    randomX = Random.Range(40, customBounds.x + 40);
+                    randomY = Random.Range(40, customBounds.y + 40);
+                    //Debug.Log($"CUSTOM X: {randomX}, Y: {randomY}");
+                    tile = grid.GetTileAtPos(new Vector2(Mathf.Round(randomX), Mathf.Round(randomY)));
+                } while (!tile.isWalkable);
                 //Debug.Log($"Tile Pos: {tile.transform.position}");
                 return tile.gameObject.transform.position;
             }
